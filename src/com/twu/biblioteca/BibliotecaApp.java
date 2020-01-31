76e4ea98879c9226bf520ff97bea31d482d5e431
Renata Scheibler <renata.davila@thoughtworks.com>
2020-01-31 10:32:18 -0300

    Add List of Books to BibliotecaApp

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,9 +3,11 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     private static String welcomeMsg = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    private static String listOfBooks = "Harry Potter\nLord of the Rings\nThe Fall of Gondolin";
 
     public static void main(String[] args) {
         System.out.println(getWelcomeMsg());
+        System.out.println(listOfBooks);
     }
 
     /**
