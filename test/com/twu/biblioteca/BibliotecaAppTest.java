26b1beefef694fdd315949682eb72997d4a1100b
Renata Scheibler <renata.davila@thoughtworks.com>
2020-01-30 22:51:29 -0300

    Add test for list of books

---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,71 @@
+package com.twu.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * Test biblioteca app.
+ * */
+public class BibliotecaAppTest {
+
+    // Welcome message that should be displayed
+    private final String welcomeMsg = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    private final String listOfBooks = "Harry Potter\nLord of the Rings\nThe Fall of Gondolin";
+
+    /**
+     * Capture the system output to be tested
+     * */
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    /**
+     * Should be possible to see the welcome message after starting the app.
+     * */
+    @Test
+    public void testUserGetsAWelcomeMessage() {
+        // GIVEN
+        // A Customer
+        // WHEN
+        // They start the application
+        BibliotecaApp.main(new String[] {""});
+
+        // THEN
+        // They see a welcome message
+        assertEquals(welcomeMsg + System.getProperty("line.separator"), outContent.toString());
+    }
+
+    /**
+     * Should be possible to see a list of all books after the welcome message.
+     * */
+    @Test
+    public void testUserGetsAListOfBooks() {
+        // GIVEN
+        // A Customer
+        // WHEN
+        // After the welcome message appears
+        BibliotecaApp.main(new String[] {""});
+        outContent.toString();
+
+        // THEN
+        // They see a list of all library books
+        assertEquals(listOfBooks + System.getProperty("line.separator"), outContent.toString());
+    }
+}
+
